941710b2f802dfa531c2062d6ce64b5783322c81
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2024-12-04 01:05:02 -0800

    Added Uart_rx file with error resolutions

--- Design/UART_RX.sv
+++ Design/UART_RX.sv
@@ -14,20 +14,20 @@ module uart_rx #(
     localparam CLK_DIVIDE = (CLK_FREQ / BAUD_RATE);
 				
     state_e rx_STATE, rx_NEXT;
-    config_st configt, nxt_config;
+    config_st rx_configt, rx_nxt_config;
 
     // Sequential logic for state and configuration updates
     always_ff @(posedge clk) begin
         if (rst) begin
             rx_STATE <= IDLE;
-            configt.clk_div <= 0;
-            configt.data <= 0;
-            configt.index_bit <= 0;
+            rx_configt.clk_div <= 0;
+            rx_configt.data <= 0;
+            rx_configt.index_bit <= 0;
         end else begin
             rx_STATE <= rx_NEXT;
-            configt.clk_div <= nxt_config.clk_div;
-            configt.data <= nxt_config.data;
-            configt.index_bit <= nxt_config.index_bit;
+            rx_configt.clk_div <= rx_nxt_config.clk_div;
+            rx_configt.data <= rx_nxt_config.data;
+            rx_configt.index_bit <= rx_nxt_config.index_bit;
         end
     end
 
@@ -35,14 +35,14 @@ module uart_rx #(
     always @(*) begin
         // Default assignments
         rx_NEXT = rx_STATE;
-        nxt_config.clk_div = configt.clk_div;
-        nxt_config.data = configt.data;
-        nxt_config.index_bit = configt.index_bit;
+        rx_nxt_config.clk_div = rx_configt.clk_div;
+        rx_nxt_config.data = rx_configt.data;
+        rx_nxt_config.index_bit = rx_configt.index_bit;
 
         case (rx_STATE)					 
             IDLE: begin
-                nxt_config.clk_div = 0;
-                nxt_config.index_bit = 0;
+                rx_nxt_config.clk_div = 0;
+                rx_nxt_config.index_bit = 0;
                 if (rx == 0) begin
                     rx_NEXT = START;
                 end else begin
@@ -51,42 +51,42 @@ module uart_rx #(
             end
 
             START: begin
-                if (configt.clk_div == (CLK_DIVIDE - 1) / 2) begin
+                if (rx_configt.clk_div == (CLK_DIVIDE - 1) / 2) begin
                     if (rx == 0) begin
-                        nxt_config.clk_div = 0;
+                        rx_nxt_config.clk_div = 0;
                         rx_NEXT = DATA;
                     end else begin
                         rx_NEXT = IDLE;
                     end
                 end else begin
-                    nxt_config.clk_div = configt.clk_div + 1'b1;
+                    rx_nxt_config.clk_div = rx_configt.clk_div + 1'b1;
                     rx_NEXT = START;
                 end
             end
 
             DATA: begin
-                if (configt.clk_div < CLK_DIVIDE - 1) begin
-                    nxt_config.clk_div = configt.clk_div + 1'b1;
+                if (rx_configt.clk_div < CLK_DIVIDE - 1) begin
+                    rx_nxt_config.clk_div = rx_configt.clk_div + 1'b1;
                     rx_NEXT = DATA;
                 end else begin
-                    nxt_config.clk_div = 0;
-                    nxt_config.data[configt.index_bit] = rx;
-                    if (configt.index_bit < 7) begin
-                        nxt_config.index_bit = nxt_config.index_bit + 1'b1;
+                    rx_nxt_config.clk_div = 0;
+                    rx_nxt_config.data[rx_configt.index_bit] = rx;
+                    if (rx_configt.index_bit < 7) begin
+                        rx_nxt_config.index_bit = rx_nxt_config.index_bit + 1'b1;
                         rx_NEXT = DATA;
                     end else begin
-                        nxt_config.index_bit = 0;
+                        rx_nxt_config.index_bit = 0;
                         rx_NEXT = STOP;
                     end
                 end
             end
 
             STOP: begin
-                if (configt.clk_div < CLK_DIVIDE - 1) begin
-                    nxt_config.clk_div = configt.clk_div + 1'b1;
+                if (rx_configt.clk_div < CLK_DIVIDE - 1) begin
+                    rx_nxt_config.clk_div = rx_configt.clk_div + 1'b1;
                     rx_NEXT = STOP;
                 end else begin
-                    nxt_config.clk_div = 0;
+                    rx_nxt_config.clk_div = 0;
                     rx_NEXT = DONE;
                 end
             end
@@ -100,7 +100,7 @@ module uart_rx #(
     end
 
     // Output assignment
-    assign rx_data_out = configt.data;
+    assign rx_data_out = rx_configt.data;
 
 endmodule
 
